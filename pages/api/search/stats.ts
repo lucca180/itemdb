aba4dba0a93f53822a11a61f057cd5f0853b04f2
Lucca <lucca180@gmail.com>
2023-03-19 19:08:30 -0300

    optimize more search by colors

--- pages/api/search/stats.ts
+++ pages/api/search/stats.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../utils/prisma';
-import { Items } from '@prisma/client';
+import { Items, Prisma } from '@prisma/client';
 import Color from 'color';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
@@ -13,36 +13,71 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
   // if(!query) return res.status(400).json({error: 'invalid search query'});
 
   const isColorSearch = query.match(/#[0-9A-Fa-f]{6}$/gm);
-  const includeIds = [];
-
-  if (isColorSearch) {
-    const x = Color(query);
-    const [l, a, b] = x.lab().array();
-
-    const resultRaw = (await prisma.$queryRaw`
-      SELECT a.internal_id
-      FROM Items as a
-      LEFT JOIN (
-          SELECT image_id, min((POWER(lab_l-${l},2)+POWER(lab_a-${a},2)+POWER(lab_b-${b},2))) as dist
-          FROM ItemColor
-          GROUP BY image_id 
-          having dist <= 750
-      ) as d on a.image_id = d.image_id
-      where d.dist is not null 
-    `) as any[];
-
-    includeIds.push(...resultRaw.map((a) => a.internal_id));
-  }
-
+  
   const groups = [
     'category',
-    // 'isNC',
     'isWearable',
     'status',
     'type',
     'isNeohome',
   ] as (keyof Items)[];
 
+  if (isColorSearch) {
+    const color = Color(query);
+    const [l, a, b] = color.lab().array();
+
+    const queries = []
+
+    for(const group of groups) {
+      let column;
+      if(group === 'isWearable')
+        column = Prisma.sql`a.isWearable`;
+      else if(group === 'isNeohome')
+        column = Prisma.sql`a.isNeohome`;
+      else if(group === 'status')
+        column = Prisma.sql`a.status`;
+      else if(group === 'type')
+        column = Prisma.sql`a.type`;
+      else
+        column = Prisma.sql`a.category`;
+
+      const sqlQuery = (prisma.$queryRaw`
+        SELECT ${column}, count(*) as count
+        FROM Items as a
+        LEFT JOIN (
+            SELECT image_id, min((POWER(lab_l-${l},2)+POWER(lab_a-${a},2)+POWER(lab_b-${b},2))) as dist
+            FROM ItemColor
+            GROUP BY image_id 
+            having dist <= 750
+        ) as d on a.image_id = d.image_id
+        where d.dist is not null
+        group by ${column}
+      `)
+
+      queries.push(sqlQuery)
+    }
+
+    const resultRaw = await Promise.all(queries);
+    const result: { [id: string]: { [id: string]: number } | number } = {};
+
+    for (let i = 0; i < groups.length; i++) {
+      const group = groups[i];
+      const groupData = resultRaw[i] as any[];
+      const x: { [id: string]: number } = {};
+
+      for (const data of groupData) {
+        let name = data[group]?.toString() || 'Unknown';
+        name = name === '0' ? 'false' : name === '1' ? 'true' : name;
+        x[name] = x[name] ? x[name] + Number(data.count) : Number(data.count);
+      }
+  
+      result[group] = x;
+    }
+
+    return res.json(result);
+  }
+
+ 
   const promises = [];
 
   for (const group of groups) {
@@ -52,14 +87,14 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
         _all: true,
       },
       where: {
-        OR: [{ name: { contains: query } }, { internal_id: { in: includeIds } }],
+        name: { contains: query } 
       },
     });
 
     promises.push(x);
   }
 
-  const promiseResult = await prisma.$transaction(promises);
+  const promiseResult = await Promise.all(promises);
 
   const result: { [id: string]: { [id: string]: number } | number } = {};
 
