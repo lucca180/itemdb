57ac3b085aba51f1e289a208c309719cc2761c1a
Lucca <lucca180@gmail.com>
2023-03-20 00:48:29 -0300

    add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,17 @@ import '../utils/global.css';
 import { initializeApp } from 'firebase/app';
 import { RecoilRoot } from 'recoil';
 import Head from 'next/head';
+import { GoogleAnalytics, event } from "nextjs-google-analytics";
+import type { NextWebVitalsMetric } from "next/app";
+
+export function reportWebVitals({ id, name, label, value }: NextWebVitalsMetric) {
+  event(name, {
+    category: label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
+    value: Math.round(name === "CLS" ? value * 1000 : value), // values must be integers
+    label: id, // id unique to current page load
+    nonInteraction: true, // avoids affecting bounce rate.
+  });
+}
 
 const isProd = process.env.NODE_ENV === 'production';
 
@@ -30,6 +41,7 @@ function MyApp({ Component, pageProps }: any) {
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
           <title>itemdb - Open Source Neopets Item Database</title>
         </Head>
+        <GoogleAnalytics trackPageViews />
         <Component {...pageProps} />
       </ChakraProvider>
     </RecoilRoot>
