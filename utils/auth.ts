3519f51df28b91dd93526efcd533128b26c025f4
Lucca <lucca180@gmail.com>
2023-09-04 23:11:35 -0300

    perf: ⚡️ dynamic import firebase
    This should improve the loading time and reduce the unused js budled :pray:

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react';
 
 import axios from 'axios';
-import { getAuth, User as FirebaseUser } from 'firebase/auth';
 import { useRouter } from 'next/router';
 import { User, UserList } from '../types';
 import { setCookie, deleteCookie } from 'cookies-next';
 import { useMutex } from 'react-context-mutex';
 import { useAtom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
+import type { User as FirebaseUser } from 'firebase/auth';
+
+const getAuth = () => import('./firebase/auth');
 
 const storage =
   typeof window !== 'undefined'
@@ -39,46 +41,52 @@ export const useAuth = (props?: UseAuthProps) => {
   const [isLoading, setIsLoading] = React.useState(true);
   const [user, setUser] = useAtom(UserState);
   const [, setLists] = useAtom(UserLists);
-  const auth = getAuth();
   const router = useRouter();
   const MutexRunner = useMutex();
   const mutex = new MutexRunner('authMutex');
 
   useEffect(() => {
-    const unsub = auth.onAuthStateChanged(async (fireUser) => {
-      if (!fireUser) {
-        setUser(null);
+    const unsubs = getAuth().then((res) => {
+      const { auth } = res;
 
-        if (redirect) router.push(redirect);
+      const unsub1 = auth.onAuthStateChanged(async (fireUser) => {
+        if (!fireUser) {
+          setUser(null);
 
-        setIsLoading(false);
-        return;
-      }
+          if (redirect) router.push(redirect);
 
-      if (user && user.id === fireUser.uid) {
-        setIsLoading(false);
-        return;
-      }
-
-      mutex.run(() => doLogin(fireUser));
-    });
-
-    const unsubToken = auth.onIdTokenChanged(async (fireUser) => {
-      if (fireUser) {
-        const token = await fireUser.getIdToken();
-
-        if (!token) {
-          deleteCookie('userToken');
+          setIsLoading(false);
           return;
         }
 
-        setCookie('userToken', token, { secure: true });
-      }
+        if (user && user.id === fireUser.uid) {
+          setIsLoading(false);
+          return;
+        }
+
+        mutex.run(() => doLogin(fireUser));
+      });
+
+      const unsub2 = auth.onIdTokenChanged(async (fireUser) => {
+        if (fireUser) {
+          const token = await fireUser.getIdToken();
+
+          if (!token) {
+            deleteCookie('userToken');
+            return;
+          }
+
+          setCookie('userToken', token, { secure: true });
+        }
+      });
+
+      return [unsub1, unsub2];
     });
 
     return () => {
-      unsub();
-      unsubToken();
+      unsubs.then((unsubs) => {
+        unsubs.forEach((unsub) => unsub());
+      });
     };
   }, []);
 
@@ -117,6 +125,7 @@ export const useAuth = (props?: UseAuthProps) => {
 
   const getIdToken = async (forceRefresh = false) => {
     try {
+      const auth = (await getAuth()).auth;
       if (!auth.currentUser) return null;
 
       const token = await auth.currentUser.getIdToken(forceRefresh);
@@ -130,7 +139,8 @@ export const useAuth = (props?: UseAuthProps) => {
     }
   };
 
-  const signout = () => {
+  const signout = async () => {
+    const auth = (await getAuth()).auth;
     auth.signOut();
     setUser(null);
     setLists(null);
@@ -143,6 +153,5 @@ export const useAuth = (props?: UseAuthProps) => {
     signout,
     getIdToken,
     setUser,
-    firebaseUser: auth.currentUser,
   };
 };
