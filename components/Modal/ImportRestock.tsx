2000732aa8df1315bd736ec194063c15e0862b8b
Lucca <lucca180@gmail.com>
2023-12-21 20:48:22 -0300

    fix: 🐛 empty import modal when first load the page

--- components/Modal/ImportRestock.tsx
+++ components/Modal/ImportRestock.tsx
@@ -36,7 +36,7 @@ const ImportRestockModal = (props: FeedbackModalProps) => {
 
   useEffect(() => {
     init();
-  }, []);
+  }, [isOpen]);
 
   const init = async () => {
     const current = sessionStorage.getItem('current_sessions');
