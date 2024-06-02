95d115bfad1f9b597f5cc0386b56cdd938aa93c5
Lucca <lucca180@gmail.com>
2024-06-02 17:00:53 -0300

    ci: 🐛 tweak pm env file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,18 +10,5 @@ module.exports = {
       merge_logs: true,
       max_memory_restart: '600M',
     },
-    {
-      name: 'itemdb-maintenance',
-      script: './node_modules/.bin/next',
-      args: 'start -p 4000',
-      instances: '1',
-      exec_mode: 'fork',
-      time: true,
-      merge_logs: true,
-      max_memory_restart: '100M',
-      env: {
-        MAINTENANCE_MODE: 'true',
-      },
-    },
   ],
 };
