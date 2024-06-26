6969c9c49c8587f15e40fca8aac6a1e0450e449c
Lucca <lucca180@gmail.com>
2024-06-26 14:28:03 -0300

    perf: 🗃️ drop restock session json column

---
+++ prisma/migrations/20240626172616_drop_json_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `session` on the `restocksession` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `restocksession` DROP COLUMN `session`;
