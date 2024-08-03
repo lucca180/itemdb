7c13c7ad32e5883f274d42faa341cae7fe44ca47
Lucca <lucca180@gmail.com>
2024-08-03 20:32:44 -0300

    perf: 🗃️ drop useless column

---
+++ prisma/migrations/20240803232920_remove_useless_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `item_id` on the `tradeitems` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `tradeitems` DROP COLUMN `item_id`;
