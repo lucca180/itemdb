9a48f85c146647f8af407a00971ed1fbb2b5559c
Lucca <lucca180@gmail.com>
2024-10-13 18:26:53 -0300

    ci: ✨ use green-blue deployment
    no more downtimes during deploys (we hope)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,5 +10,15 @@ module.exports = {
       merge_logs: true,
       max_memory_restart: '1000M',
     },
+    {
+      name: 'itemdb-green',
+      script: './node_modules/.bin/next',
+      args: 'start -p 4000',
+      instances: '1',
+      exec_mode: 'fork',
+      time: true,
+      merge_logs: true,
+      max_memory_restart: '1000M',
+    },
   ],
 };
