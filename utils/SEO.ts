6e413902caf7f612320278dcf5c6285c5a7a3742
Lucca <lucca180@gmail.com>
2025-03-08 13:12:40 -0300

    feat: 🔍️ improve seo description

--- utils/SEO.ts
+++ utils/SEO.ts
@@ -4,7 +4,7 @@ const SEOConfig: DefaultSeoProps = {
   defaultTitle: 'itemdb - Neopets Item Database',
   titleTemplate: '%s | itemdb - Neopets Item Database',
   description:
-    'Find all the data about Neopets items including the most updated prices, wearable previews, restock history, color search, mystery capsule drop rates, and more! Create your item lists easily and share around Neopia!',
+    'Explore Neopets items: updated prices, wearable previews, restock history, color search & mystery capsule drop rates. Build, save and share lists in Neopia!',
   openGraph: {
     type: 'website',
     images: [
@@ -21,6 +21,10 @@ const SEOConfig: DefaultSeoProps = {
 
 export const getDefaultSEO = (locale: string): DefaultSeoProps => ({
   ...SEOConfig,
+  description:
+    locale === 'pt'
+      ? 'Explore itens do Neopets: preços atualizados, prévias de roupas, histórico de restock, busca por cores e mais. Crie, salve e compartilhe listas em Neopia!'
+      : SEOConfig.description,
   openGraph: {
     ...SEOConfig.openGraph,
     locale: locale === 'pt' ? 'pt_BR' : 'en_US',
