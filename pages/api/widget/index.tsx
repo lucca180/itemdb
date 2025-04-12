a01b93fee30cca1334f7cb5de0110dd1b5d13fe4
Lucca <lucca180@gmail.com>
2025-04-12 15:06:08 -0300

    fix: 🐛 return widget as text to prevent adding cloudflare beacon

--- pages/api/widget/index.tsx
+++ pages/api/widget/index.tsx
@@ -33,6 +33,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // res.setHeader('Access-Control-Allow-Origin', '*');
   // res.setHeader('Access-Control-Allow-Methods', 'GET');
   // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Type', 'text/plain');
   res.status(200).send(html);
 }
