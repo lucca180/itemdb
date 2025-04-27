854cfac8201bcca81648ecc84cc22c76eaaae588
Lucca <lucca180@gmail.com>
2025-04-27 12:38:26 -0300

    fix: 🧑‍💻 don't use tree-shake intl on dev

--- utils/load-translation.ts
+++ utils/load-translation.ts
@@ -1,4 +1,7 @@
 import { loadTranslation as _loadTranslation } from '@utils/intlHandler';
 
 // wanted to move this code to the intlHandler.ts but without updating EVERY import in the codebase
-export const loadTranslation = _loadTranslation;
+export const loadTranslation = (locale: string, relativePath: string) =>
+  process.env.NODE_ENV === 'development'
+    ? import(`../translation/${locale}.json`).then((res) => res.default)
+    : _loadTranslation(locale, relativePath);
