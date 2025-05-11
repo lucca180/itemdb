b7c984268bfbffae743207e662af8cec465ff499
Lucca <lucca180@gmail.com>
2025-05-11 19:00:17 -0300

    fix: 🚑️ hotfix instrumentation

--- instrumentation.ts
+++ instrumentation.ts
@@ -6,23 +6,44 @@ export function register() {
   const isProd = process.env.NODE_ENV === 'production';
 
   if (!isProd) return;
-
-  Sentry.init({
-    dsn:
-      SENTRY_DSN ||
-      'https://d093bca7709346a6a45966764e1b1988@o1042114.ingest.us.sentry.io/4504761196216321',
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 0.3,
-    profilesSampleRate: 0.5,
-    integrations: [
-      Sentry.prismaIntegration(),
-      Sentry.captureConsoleIntegration({
-        // array of methods that should be captured
-        // defaults to ['log', 'info', 'warn', 'error', 'debug', 'assert']
-        levels: ['error'],
-      }),
-    ],
-  });
+  try {
+    if (process.env.NEXT_RUNTIME === 'nodejs') {
+      Sentry.init({
+        dsn:
+          SENTRY_DSN ||
+          'https://d093bca7709346a6a45966764e1b1988@o1042114.ingest.us.sentry.io/4504761196216321',
+        // Adjust this value in production, or use tracesSampler for greater control
+        tracesSampleRate: 0.3,
+        profilesSampleRate: 0.5,
+        integrations: [
+          Sentry.prismaIntegration(),
+          Sentry.captureConsoleIntegration({
+            // array of methods that should be captured
+            // defaults to ['log', 'info', 'warn', 'error', 'debug', 'assert']
+            levels: ['error'],
+          }),
+        ],
+      });
+    } else {
+      Sentry.init({
+        dsn:
+          SENTRY_DSN ||
+          'https://d093bca7709346a6a45966764e1b1988@o1042114.ingest.us.sentry.io/4504761196216321',
+        // Adjust this value in production, or use tracesSampler for greater control
+        tracesSampleRate: 0.3,
+        profilesSampleRate: 0.5,
+        integrations: [
+          Sentry.captureConsoleIntegration({
+            // array of methods that should be captured
+            // defaults to ['log', 'info', 'warn', 'error', 'debug', 'assert']
+            levels: ['error'],
+          }),
+        ],
+      });
+    }
+  } catch (error) {
+    console.error('Error initializing Sentry:', error);
+  }
 }
 
 export const onRequestError = Sentry.captureRequestError;
