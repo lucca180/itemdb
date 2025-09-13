83ce515f16981ecd278e848167429c833e4979fc
Lucca <lucca180@gmail.com>
2025-09-13 20:26:37 -0300

    fix: 🚑️ disable query compiler for mariadb migration

--- utils/prisma.ts
+++ utils/prisma.ts
@@ -8,35 +8,37 @@ const globalForPrisma = global as unknown as {
 
 const dbInfo = parseMysql(process.env.DATABASE_URL || '');
 
-const adapter =
-  globalForPrisma.adapter ||
-  new PrismaMariaDb({
-    user: dbInfo.user,
-    password: dbInfo.password,
-    host: dbInfo.host,
-    port: dbInfo.port,
-    database: dbInfo.database,
-    connectionLimit: dbInfo.queryParams.connectionLimit
-      ? parseInt(dbInfo.queryParams.connectionLimit, 10)
-      : 5,
-    acquireTimeout: dbInfo.queryParams.pool_timeout
-      ? parseInt(dbInfo.queryParams.pool_timeout, 10) * 1000
-      : 30000,
-  });
+// const adapter =
+//   globalForPrisma.adapter ||
+//   new PrismaMariaDb({
+//     user: dbInfo.user,
+//     password: dbInfo.password,
+//     host: dbInfo.host,
+//     port: dbInfo.port,
+//     database: dbInfo.database,
+//     connectionLimit: dbInfo.queryParams.connectionLimit
+//       ? parseInt(dbInfo.queryParams.connectionLimit, 10)
+//       : 5,
+//     acquireTimeout: dbInfo.queryParams.pool_timeout
+//       ? parseInt(dbInfo.queryParams.pool_timeout, 10) * 1000
+//       : 30000,
+//   });
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    adapter,
-    transactionOptions: {
-      maxWait: 5000,
-      timeout: 10000,
-    },
-  });
+// const prisma =
+//   globalForPrisma.prisma ||
+//   new PrismaClient({
+//     adapter,
+//     transactionOptions: {
+//       maxWait: 5000,
+//       timeout: 10000,
+//     },
+//   });
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
-  globalForPrisma.adapter = adapter;
+  // globalForPrisma.adapter = adapter;
 }
 
 export default prisma;
