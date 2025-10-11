f26589ce3cd506bad622edb5682bedd72ca811ed
Lucca <lucca180@gmail.com>
2025-10-11 15:24:40 -0300

    fix: 🐛 build error

--- components/Hubs/Wrapped2024/Timeline.tsx
+++ components/Hubs/Wrapped2024/Timeline.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @next/next/no-css-tags */
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
 import { RestockStats } from '../../../types';
 import { useFormatter } from 'next-intl';
 import { FaCalendar } from 'react-icons/fa';
@@ -19,126 +19,129 @@ const WrappedTimeline = (props: WrappedTimelineProps) => {
   const format = useFormatter();
 
   return (
-    <VerticalTimeline>
-      <VerticalTimelineElement
-        contentStyle={{ background: mainColor, color: '#fff' }}
-        date={format.dateTime(wrappedData.startDate, {
-          day: 'numeric',
-          month: 'long',
-          year: 'numeric',
-        })}
-        iconStyle={{ background: mainColor, color: '#fff' }}
-        icon={<FaCalendar />}
-      >
-        <Flex>
-          <Text as="span">Our journey begins here</Text>
-        </Flex>
-      </VerticalTimelineElement>
-      {monthlyData.map((month) => (
+    <>
+      <link rel="stylesheet" href="/css/vertical-timeline.css" />
+      <VerticalTimeline>
         <VerticalTimelineElement
-          key={month.startDate}
-          date={format.dateTime(
-            isLastDayOfMonth(month.startDate) ? addDays(month.startDate, 1) : month.startDate,
-            {
-              month: 'long',
-            }
-          )}
           contentStyle={{ background: mainColor, color: '#fff' }}
+          date={format.dateTime(wrappedData.startDate, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          })}
           iconStyle={{ background: mainColor, color: '#fff' }}
           icon={<FaCalendar />}
         >
-          <Flex flexFlow={'column'} alignItems={'center'}>
-            <Flex gap={3}>
-              <Text
-                bg="blackAlpha.400"
-                p={2}
-                borderRadius={'md'}
-                fontSize="sm"
-                as="span"
-                textAlign={'center'}
-                display={'inline-block'}
-              >
-                Est. Revenue
-                <br />
-                <b>{format.number(month.estRevenue)} NP</b>
-              </Text>
-              <Text
-                bg="blackAlpha.400"
-                p={2}
-                borderRadius={'md'}
-                fontSize="sm"
-                as="span"
-                textAlign={'center'}
-                display={'inline-block'}
-              >
-                Items Bought <br />
-                <b>{format.number(month.totalBought.count)}</b>
-              </Text>
-              <Text
-                bg="blackAlpha.400"
-                p={2}
-                borderRadius={'md'}
-                fontSize="sm"
-                as="span"
-                textAlign={'center'}
-                display={'inline-block'}
-              >
-                Total Lost <br />
-                <b>{format.number(month.totalLost.value)} NP</b>
-              </Text>
-            </Flex>
-            <Tabs variant="soft-rounded" colorScheme="green" align="center" mt={3}>
-              <TabList>
-                <Tab color={'white'}>Hottest Buys</Tab>
-                <Tab color={'white'}>Worst Losses</Tab>
-              </TabList>
-              <TabPanels>
-                <TabPanel>
-                  <Flex flexWrap={'wrap'} flexFlow={'column'} gap={2}>
-                    {month.hottestBought.slice(0, 5).map((bought, i) => (
-                      <RestockItem
-                        disablePrefetch
-                        item={bought.item}
-                        clickData={bought.click}
-                        restockItem={bought.restockItem}
-                        key={i}
-                      />
-                    ))}
-                  </Flex>
-                </TabPanel>
-                <TabPanel>
-                  <Flex flexWrap={'wrap'} flexFlow={'column'} gap={2} mt={2}>
-                    {month.hottestLost.slice(0, 5).map((bought, i) => (
-                      <RestockItem
-                        disablePrefetch
-                        item={bought.item}
-                        clickData={bought.click}
-                        restockItem={bought.restockItem}
-                        key={i}
-                      />
-                    ))}
-                  </Flex>
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
+          <Flex>
+            <Text as="span">Our journey begins here</Text>
           </Flex>
         </VerticalTimelineElement>
-      ))}
-      <VerticalTimelineElement
-        contentStyle={{ background: mainColor, color: '#fff' }}
-        date={format.dateTime(wrappedData.endDate, {
-          day: 'numeric',
-          month: 'long',
-          year: 'numeric',
-        })}
-        iconStyle={{ background: mainColor, color: '#fff' }}
-        icon={<FaCalendar />}
-      >
-        <Flex>
-          <Text as="span">Our journey ends here</Text>
-        </Flex>
-      </VerticalTimelineElement>
-    </VerticalTimeline>
+        {monthlyData.map((month) => (
+          <VerticalTimelineElement
+            key={month.startDate}
+            date={format.dateTime(
+              isLastDayOfMonth(month.startDate) ? addDays(month.startDate, 1) : month.startDate,
+              {
+                month: 'long',
+              }
+            )}
+            contentStyle={{ background: mainColor, color: '#fff' }}
+            iconStyle={{ background: mainColor, color: '#fff' }}
+            icon={<FaCalendar />}
+          >
+            <Flex flexFlow={'column'} alignItems={'center'}>
+              <Flex gap={3}>
+                <Text
+                  bg="blackAlpha.400"
+                  p={2}
+                  borderRadius={'md'}
+                  fontSize="sm"
+                  as="span"
+                  textAlign={'center'}
+                  display={'inline-block'}
+                >
+                  Est. Revenue
+                  <br />
+                  <b>{format.number(month.estRevenue)} NP</b>
+                </Text>
+                <Text
+                  bg="blackAlpha.400"
+                  p={2}
+                  borderRadius={'md'}
+                  fontSize="sm"
+                  as="span"
+                  textAlign={'center'}
+                  display={'inline-block'}
+                >
+                  Items Bought <br />
+                  <b>{format.number(month.totalBought.count)}</b>
+                </Text>
+                <Text
+                  bg="blackAlpha.400"
+                  p={2}
+                  borderRadius={'md'}
+                  fontSize="sm"
+                  as="span"
+                  textAlign={'center'}
+                  display={'inline-block'}
+                >
+                  Total Lost <br />
+                  <b>{format.number(month.totalLost.value)} NP</b>
+                </Text>
+              </Flex>
+              <Tabs variant="soft-rounded" colorScheme="green" align="center" mt={3}>
+                <TabList>
+                  <Tab color={'white'}>Hottest Buys</Tab>
+                  <Tab color={'white'}>Worst Losses</Tab>
+                </TabList>
+                <TabPanels>
+                  <TabPanel>
+                    <Flex flexWrap={'wrap'} flexFlow={'column'} gap={2}>
+                      {month.hottestBought.slice(0, 5).map((bought, i) => (
+                        <RestockItem
+                          disablePrefetch
+                          item={bought.item}
+                          clickData={bought.click}
+                          restockItem={bought.restockItem}
+                          key={i}
+                        />
+                      ))}
+                    </Flex>
+                  </TabPanel>
+                  <TabPanel>
+                    <Flex flexWrap={'wrap'} flexFlow={'column'} gap={2} mt={2}>
+                      {month.hottestLost.slice(0, 5).map((bought, i) => (
+                        <RestockItem
+                          disablePrefetch
+                          item={bought.item}
+                          clickData={bought.click}
+                          restockItem={bought.restockItem}
+                          key={i}
+                        />
+                      ))}
+                    </Flex>
+                  </TabPanel>
+                </TabPanels>
+              </Tabs>
+            </Flex>
+          </VerticalTimelineElement>
+        ))}
+        <VerticalTimelineElement
+          contentStyle={{ background: mainColor, color: '#fff' }}
+          date={format.dateTime(wrappedData.endDate, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+          })}
+          iconStyle={{ background: mainColor, color: '#fff' }}
+          icon={<FaCalendar />}
+        >
+          <Flex>
+            <Text as="span">Our journey ends here</Text>
+          </Flex>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </>
   );
 };
 
