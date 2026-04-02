e329da61b8589a07ad224f27bb2755c8d2cfedf9
Lucca <lucca180@gmail.com>
2026-04-02 20:41:07 -0300

    fix: 🐛 update user last_login

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -5,6 +5,7 @@ import { signSession, needsRefresh, SESSION_DURATION_SECONDS } from '@utils/auth
 import { rawToUser } from './login';
 import { UserRoles } from '../../../types';
 import prisma from '@utils/prisma';
+import requestIp from 'request-ip';
 
 export default async function handle(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') return res.status(405).json({ error: 'Method not allowed' });
@@ -21,11 +22,22 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
         email: user.email,
         role: user.role as UserRoles,
       });
+
       res.setHeader(
         'Set-Cookie',
         `session=${newToken};Path=/;HttpOnly;Secure;SameSite=Strict;Max-Age=${SESSION_DURATION_SECONDS};`
       );
     }
+
+    const ip = requestIp.getClientIp(req) || '';
+
+    await prisma.user.update({
+      where: { id: user.id },
+      data: {
+        last_ip: ip,
+        last_login: new Date(),
+      },
+    });
 
     return res.json(user);
   } catch {
