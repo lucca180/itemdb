6e1574511e8d78be997fd4aa66034cb3d769bd6f
Lucca <lucca180@gmail.com>
2026-05-25 21:08:03 -0300

    fix: 🐛 disable intl tree-shake during app router migration

--- utils/load-translation.ts
+++ utils/load-translation.ts
@@ -1,85 +1,87 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 
-const treeShakeExists = () => {
-  return fs.existsSync(path.resolve(process.cwd(), 'translation/tree-shake/'));
-};
+// const treeShakeExists = () => {
+//   return fs.existsSync(path.resolve(process.cwd(), 'translation/tree-shake/'));
+// };
 
 // wanted to move this code to the intlHandler.ts but without updating EVERY import in the codebase
-export const loadTranslation = (locale: string, relativePath: string, isApp = false) => {
-  return process.env.NODE_ENV === 'development' || !treeShakeExists()
-    ? import(`../translation/${locale}.json`).then((res) => res.default)
-    : _loadTranslation(locale, relativePath, isApp);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const loadTranslation = (locale: string, _relativePath: string, _isApp = false) => {
+  return import(`../translation/${locale}.json`).then((res) => res.default);
+  // return process.env.NODE_ENV === 'development' || !treeShakeExists()
+  //   ? import(`../translation/${locale}.json`).then((res) => res.default)
+  //   : _loadTranslation(locale, relativePath, isApp);
 };
 
-const _loadTranslation = async (locale: string, relativePath: string, isApp: boolean) => {
-  if (!locale) locale = 'en';
+// const _loadTranslation = async (locale: string, relativePath: string, isApp: boolean) => {
+//   if (!locale) locale = 'en';
 
-  let translations: {
-    [namespace: string]: { [key: string]: string };
-  } = {};
+//   let translations: {
+//     [namespace: string]: { [key: string]: string };
+//   } = {};
 
-  let treeshake: {
-    keys: string[];
-    namespaces: string[];
-  } = {
-    keys: [],
-    namespaces: [],
-  };
-  if (process.env.NODE_ENV === 'development') return;
-  try {
-    [translations, treeshake] = (await Promise.all([
-      import(`../translation/${locale}.json`),
-      import(`../translation/tree-shake/${isApp ? 'app' : 'pages'}/${relativePath}.json`),
-    ])) as [
-      {
-        [namespace: string]: { [key: string]: string };
-      },
-      {
-        keys: string[];
-        namespaces: string[];
-      },
-    ];
+//   let treeshake: {
+//     keys: string[];
+//     namespaces: string[];
+//   } = {
+//     keys: [],
+//     namespaces: [],
+//   };
+//   if (process.env.NODE_ENV === 'development') return;
+//   try {
+//     [translations, treeshake] = (await Promise.all([
+//       import(`../translation/${locale}.json`),
+//       import(`../translation/tree-shake/${isApp ? 'app' : 'pages'}/${relativePath}.json`),
+//     ])) as [
+//       {
+//         [namespace: string]: { [key: string]: string };
+//       },
+//       {
+//         keys: string[];
+//         namespaces: string[];
+//       },
+//     ];
 
-    if (!relativePath || !treeshake) return translations;
+//     if (!relativePath || !treeshake) return translations;
 
-    const result = {} as {
-      [namespace: string]: { [key: string]: string };
-    };
+//     const result = {} as {
+//       [namespace: string]: { [key: string]: string };
+//     };
 
-    treeshake.namespaces.forEach((namespace) => {
-      if (!translations[namespace]) {
-        console.error('Missing namespace', namespace, 'in translation file', locale);
-        return;
-      }
-      result[namespace] = translations[namespace];
-    });
+//     treeshake.namespaces.forEach((namespace) => {
+//       if (!translations[namespace]) {
+//         console.error('Missing namespace', namespace, 'in translation file', locale);
+//         return;
+//       }
+//       result[namespace] = translations[namespace];
+//     });
 
-    treeshake.keys.forEach((key) => {
-      if (key === '[DYNAMIC_KEY]') return;
+//     treeshake.keys.forEach((key) => {
+//       if (key === '[DYNAMIC_KEY]') return;
 
-      const [namespace, ...rest] = key.split('.');
+//       const [namespace, ...rest] = key.split('.');
 
-      if (treeshake.namespaces.includes(namespace)) return;
+//       if (treeshake.namespaces.includes(namespace)) return;
 
-      const translation = translations[namespace]?.[rest.join('.')];
+//       const translation = translations[namespace]?.[rest.join('.')];
 
-      if (translation) {
-        result[namespace] = {
-          ...result[namespace],
-          [rest.join('.')]: translation,
-        };
-      }
-    });
-    return result;
-  } catch (e) {
-    console.error('⚠️ Using default file for ', relativePath);
+//       if (translation) {
+//         result[namespace] = {
+//           ...result[namespace],
+//           [rest.join('.')]: translation,
+//         };
+//       }
+//     });
+//     return result;
+//   } catch (e) {
+//     console.error('⚠️ Using default file for ', relativePath);
 
-    const translations = await import(`../translation/${locale}.json`).then((res) => res.default);
+//     const translations = await import(`../translation/${locale}.json`).then((res) => res.default);
 
-    return translations;
-  }
-};
+//     return translations;
+//   }
+// };
 
 export const appLoadTranslation = async (locale: string) => {
   const translations = await import(`../translation/${locale}.json`).then((res) => res.default);
