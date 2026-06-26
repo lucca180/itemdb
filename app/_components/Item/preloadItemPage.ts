bcde2e3dfe070ef2fb1beda5632ab7920d14f440
Lucca <lucca180@gmail.com>
2026-06-26 19:39:29 -0300

    fix: 🐛 add p-map preload

--- app/_components/Item/preloadItemPage.ts
+++ app/_components/Item/preloadItemPage.ts
@@ -39,51 +39,57 @@ import {
   loadTradeLists,
 } from '@app/_components/Item/loadUtils';
 import { loadSimilarItemData } from '@app/_components/Item/SimilarItems/loadSimilarItems';
+import pMap from 'p-map';
 
-function preload(promise: Promise<unknown>) {
-  void promise;
-}
+const ITEM_PAGE_PRELOAD_CONCURRENCY = 6;
 
 /** Starts item-page data fetches early; sections reuse the same cached loaders. */
 export function preloadItemPageData(item: ItemData): void {
+  const tasks: Array<() => Promise<unknown>> = [];
+  const preload = (task: () => Promise<unknown>) => {
+    tasks.push(task);
+  };
+
   const includeTrade = needsTradeLists(item);
 
-  preload(getOfficialItemLists(item.internal_id, includeTrade));
-  preload(loadItemEffects(item));
-  preload(loadItemColors(item));
+  preload(() => getOfficialItemLists(item.internal_id, includeTrade));
+  preload(() => loadItemEffects(item));
+  preload(() => loadItemColors(item));
 
   if (needsNPPrices(item)) {
-    preload(loadNPPrices(item.internal_id));
+    preload(() => loadNPPrices(item.internal_id));
   }
 
   if (needsNCTrade(item)) {
-    preload(loadNCTradeInsights(item.internal_id));
-    if (includeTrade) preload(loadTradeLists(item));
+    preload(() => loadNCTradeInsights(item.internal_id));
+    if (includeTrade) preload(() => loadTradeLists(item));
     if (needsLebronTradeHistory(item)) {
-      preload(loadLebronTradeHistory(item.internal_id, item.name));
+      preload(() => loadLebronTradeHistory(item.internal_id, item.name));
     }
   }
 
   if (needsNCMall(item)) {
-    preload(loadNCMallData(item.internal_id));
+    preload(() => loadNCMallData(item.internal_id));
   }
 
   if (needsDrops(item) || needsOutfitSection(item)) {
-    preload(loadItemOpenableMeta(item));
+    preload(() => loadItemOpenableMeta(item));
   }
 
-  if (needsPetpet(item)) preload(loadPetpetData(item.internal_id));
-  if (needsRecipes(item)) preload(loadItemRecipes(item.internal_id));
-  if (needsDye(item)) preload(loadDyeData(item.internal_id));
-  if (needsMME(item)) preload(loadMMEData(item.internal_id));
-  if (needsAuctionCard(item)) preload(loadItemAuctions(item.internal_id));
+  if (needsPetpet(item)) preload(() => loadPetpetData(item.internal_id));
+  if (needsRecipes(item)) preload(() => loadItemRecipes(item.internal_id));
+  if (needsDye(item)) preload(() => loadDyeData(item.internal_id));
+  if (needsMME(item)) preload(() => loadMMEData(item.internal_id));
+  if (needsAuctionCard(item)) preload(() => loadItemAuctions(item.internal_id));
   if (needsTradeCard(item)) {
-    preload(loadItemTrades(item.internal_id, shouldShowTradeRelisting(item)));
+    preload(() => loadItemTrades(item.internal_id, shouldShowTradeRelisting(item)));
   }
-  if (needsRestockLastSeen(item)) preload(loadLastSeen(item.internal_id));
-  if (needsWearableData(item)) preload(loadItemWearableData(item.internal_id));
+  if (needsRestockLastSeen(item)) preload(() => loadLastSeen(item.internal_id));
+  if (needsWearableData(item)) preload(() => loadItemWearableData(item.internal_id));
 
-  preload(loadSimilarItemData(item));
-  preload(loadItemParentData(item.internal_id));
-  preload(loadAvyData(item.internal_id, includeTrade));
+  preload(() => loadSimilarItemData(item));
+  preload(() => loadItemParentData(item.internal_id));
+  preload(() => loadAvyData(item.internal_id, includeTrade));
+
+  void pMap(tasks, (task) => task(), { concurrency: ITEM_PAGE_PRELOAD_CONCURRENCY });
 }
