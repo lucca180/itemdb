ba2319ca4824ebceae6cd10b315e258a5a66d211
Lucca <lucca180@gmail.com>
2026-08-26 11:36:04 -0300

    feat: ✨ improve layout items

--- components/Layout/layoutData.ts
+++ components/Layout/layoutData.ts
@@ -74,13 +74,13 @@ export function getLayoutNavSections(t: LayoutTranslate): LayoutNavSection[] {
       label: 'Mall',
       href: '/mall',
       options: [
-        { label: t('Layout.nc-mall-hub'), href: '/mall' },
+        { label: t('Layout.nc-mall-hub'), href: '/mall', newUntil: 1788825599000 },
         { label: t('NcMall.keep-reading-pet-styles'), href: '/rainbow-pool/pet-styles' },
         { label: t('HomePage.leaving-nc-mall'), href: '/mall/leaving' },
         { label: t('Layout.report-your-nc-trades'), href: '/mall/report' },
         {
-          label: t('Layout.browse-nc-mall'),
-          href: '/search?s=&type[]=ncBuyable&sortBy=added&sortDir=desc',
+          label: t('Layout.import-wishlist'),
+          href: '/lists/import/advanced',
         },
         { label: t('NcMall.keep-reading-lebron'), href: '/articles/lebron' },
       ],
@@ -91,7 +91,7 @@ export function getLayoutNavSections(t: LayoutTranslate): LayoutNavSection[] {
       options: [
         { label: t('Layout.sdb-pricer'), href: '/articles/userscripts' },
         { label: t('Layout.rainbow-pool-tool'), href: '/rainbow-pool' },
-        { label: 'Pet Styles Tokens', href: '/rainbow-pool/pet-styles', newUntil: 1786849199000 },
+        { label: 'Pet Styles Tokens', href: '/rainbow-pool/pet-styles' },
         { label: t('Layout.item-effects'), href: '/hub/item-effects' },
         { label: t('Layout.restock-dashboard'), href: '/restock/dashboard' },
       ],
