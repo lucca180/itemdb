0d6f43bae96d541c4bf766d752af4ea9bb223835
Lucca <lucca180@gmail.com>
2026-09-13 16:11:06 -0300

    ci: 💚 use jemalloc

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,10 @@ module.exports = {
       merge_logs: true,
       max_memory_restart: '3200M',
       kill_timeout: 15_000,
+      env: {
+        NODE_ENV: 'production',
+        LD_PRELOAD: '/lib/aarch64-linux-gnu/libjemalloc.so.2',
+      },
     },
     {
       name: 'itemdb-green',
@@ -21,6 +25,10 @@ module.exports = {
       merge_logs: true,
       max_memory_restart: '3200M',
       kill_timeout: 15_000,
+      env: {
+        NODE_ENV: 'production',
+        LD_PRELOAD: '/lib/aarch64-linux-gnu/libjemalloc.so.2',
+      },
     },
   ],
 };
